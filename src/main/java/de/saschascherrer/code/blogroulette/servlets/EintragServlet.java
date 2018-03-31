b5337173fa14599cfc014d8eea803158a14d4edf
cor-aurum <github@felix-schuetze.de>
2018-03-31 20:26:13 +0200

    wah->blogroulette

--- src/main/java/de/saschascherrer/code/blogroulette/servlets/EintragServlet.java
+++ src/main/java/de/saschascherrer/code/blogroulette/servlets/EintragServlet.java
@@ -1,4 +1,4 @@
-package org.dhbw.mosbach.ai.javaee;
+package de.saschascherrer.code.blogroulette.servlets;
 
 import java.io.IOException;
 import java.util.stream.Collectors;
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class HelloWorldServlet extends HttpServlet
+public class EintragServlet extends HttpServlet
 {
 	/**
 	 *
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,8 +1,8 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+<h2>Blogroulette</h2>
 
-<a href="hello?Param1=param1val&Param2=param2val1&PAram2=param2val2&myparam=echo%20echo%20echo">hello test</a>
+<a href="eintrag?Param1=param1val&Param2=param2val1&PAram2=param2val2&myparam=echo%20echo%20echo">Einträge</a>
 
 </body>
 </html>
