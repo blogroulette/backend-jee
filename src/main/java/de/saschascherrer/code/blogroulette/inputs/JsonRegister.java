812d8a99a1a44605f7d2c67d6e0a5bd977ccb934
cor-aurum <github@felix-schuetze.de>
2018-04-05 10:49:11 +0200

    pom angepasst

---
+++ src/main/java/de/saschascherrer/code/blogroulette/inputs/JsonRegister.java
@@ -0,0 +1,5 @@
+package de.saschascherrer.code.blogroulette.inputs;
+
+public class JsonRegister {
+
+}
--- src/main/java/de/saschascherrer/code/blogroulette/util/Status.java
+++ src/main/java/de/saschascherrer/code/blogroulette/util/Status.java
@@ -1,24 +1,38 @@
 package de.saschascherrer.code.blogroulette.util;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+
 public class Status implements Sendable {
-	
+
 	private String status;
-	private String error="";
+	private String error = "";
 
 	public Status(String status) {
 		this.status = status;
 	}
-	
+
 	public Status(String status, String error) {
 		this.status = status;
-		this.error=error;
+		this.error = error;
 	}
 
 	@Override
 	public String getJson() {
-		if(error.isEmpty())
-		return "{\"status\":\""+status+"\"}";
-		return "{\"status\":\""+status+"\",\"error\":\""+error+"\"}";
+		if (error.isEmpty())
+			return "{\"status\":\"" + status + "\"}";
+		return "{\"status\":\"" + status + "\",\"error\":\"" + error + "\"}";
+	}
+
+	public void writeToOut(HttpServletResponse resp) throws IOException {
+		resp.setContentType("application/json");
+		PrintWriter out = resp.getWriter();
+		out.print(getJson());
+		out.flush();
+		if (!error.isEmpty())
+			resp.sendError(400, error);
 	}
 
 }
