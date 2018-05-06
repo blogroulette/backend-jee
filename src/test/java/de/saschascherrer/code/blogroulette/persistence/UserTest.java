79d159b355dfdd30def31cafbfcae365e65e7add
JudBer <berjud2695@gmail.com>
2018-05-06 20:20:55 +0200

    JUnit 3 zu JUnit 4 in pom.xml geändert & erste Testklasse angelegt

---
+++ src/test/java/de/saschascherrer/code/blogroulette/persistence/UserTest.java
@@ -0,0 +1,14 @@
+package de.saschascherrer.code.blogroulette.persistence;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+
+}
