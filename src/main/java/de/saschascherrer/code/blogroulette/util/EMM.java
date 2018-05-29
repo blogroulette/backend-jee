627774ab00173da300144d17591c679cc5da1575
Sascha Scherrer <dev@saschascherrer.de>
2018-05-29 21:07:34 +0200

    Remove Logging from util/EMM
    Signed-off-by: Sascha Scherrer <dev@saschascherrer.de>

--- src/main/java/de/saschascherrer/code/blogroulette/util/EMM.java
+++ src/main/java/de/saschascherrer/code/blogroulette/util/EMM.java
@@ -2,7 +2,6 @@ package de.saschascherrer.code.blogroulette.util;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.logging.Logger;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -16,35 +15,30 @@ public class EMM {
     static {
         Map<String, String> environment = System.getenv();
         for (String envName : environment.keySet()) {
-            Logger.getAnonymousLogger().info("ENV: " + environment.keySet());
             System.out.printf("'%s'='%s' \n", envName, environment.get(envName));
         }
 
         properties = new HashMap<String, String>();
 
         if (environment.keySet().contains("BLOGROULETTE_DB_URL")) {
-            Logger.getAnonymousLogger().info("DB_URL from ENV:" + environment.get("BLOGROULETTE_DB_URL"));
             properties.put("javax.persistence.jdbc.url", environment.get("BLOGROULETTE_DB_URL"));
         } else {
             properties.put("javax.persistence.jdbc.url", "jdbc:mysql://localhost:3306/blogroulette");
         }
 
         if (environment.keySet().contains("BLOGROULETTE_DB_DRIVER")) {
-            Logger.getAnonymousLogger().info("DB_DRIVER from ENV:" + environment.get("BLOGROULETTE_DB_DRIVER"));
             properties.put("javax.persistence.jdbc.driver", environment.get("BLOGROULETTE_DB_DRIVER"));
         } else {
             properties.put("javax.persistence.jdbc.driver", "org.mariadb.jdbc.Driver");
         }
 
         if (environment.keySet().contains("BLOGROULETTE_DB_USER")) {
-            Logger.getAnonymousLogger().info("DB_USER from ENV:" + environment.get("BLOGROULETTE_DB_USER"));
             properties.put("javax.persistence.jdbc.user", environment.get("BLOGROULETTE_DB_USER"));
         } else {
             properties.put("javax.persistence.jdbc.user", "blogroulette");
         }
 
         if (environment.keySet().contains("BLOGROULETTE_DB_PASSWORD")) {
-            Logger.getAnonymousLogger().info("DB_PASSWORD from ENV: [exists]");
             properties.put("javax.persistence.jdbc.password", environment.get("BLOGROULETTE_DB_PASSWORD"));
         } else {
             properties.put("javax.persistence.jdbc.password", "blogroulettedefaultpassword");
