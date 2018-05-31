fa3e82457761b5d0ac119ee3c328cd0b4792f056
Felix Schütze <dev@felix-schuetze.de>
2018-05-31 16:56:13 +0200

    Error

--- src/main/java/de/saschascherrer/code/blogroulette/util/Status.java
+++ src/main/java/de/saschascherrer/code/blogroulette/util/Status.java
@@ -7,32 +7,35 @@ import javax.servlet.http.HttpServletResponse;
 
 public class Status implements Sendable {
 
-	private String status;
-	private String error = "";
+    private String status;
+    private String error = "";
 
-	public Status(String status) {
-		this.status = status;
-	}
+    public Status(String status) {
+        this.status = status;
+    }
 
-	public Status(String status, String error) {
-		this.status = status;
-		this.error = error;
-	}
+    public Status(String status, String error) {
+        this.status = status;
+        this.error = error;
+    }
 
-	@Override
-	public String getJson() {
-		if (error.isEmpty())
-			return "{\"status\":\"" + status + "\"}";
-		return "{\"status\":\"" + status + "\",\"error\":\"" + error + "\"}";
-	}
+    @Override
+    public String getJson() {
+        if (error.isEmpty())
+            return "{\"status\":\"" + status + "\"}";
+        return "{\"status\":\"" + status + "\",\"error\":\"" + error + "\"}";
+    }
 
-	public void writeToOut(HttpServletResponse resp) throws IOException {
-		resp.setContentType("application/json");
-		PrintWriter out = resp.getWriter();
-		out.print(getJson());
-		out.flush();
-		if (!error.isEmpty())
-			resp.sendError(400, error);
-	}
+    public void writeToOut(HttpServletResponse resp) throws IOException {
+        resp.setContentType("application/json");
+        PrintWriter out = resp.getWriter();
+
+        if (!error.isEmpty())
+            resp.sendError(400, error);
+        else {
+            out.print(getJson());
+        }
+        out.flush();
+    }
 
 }
